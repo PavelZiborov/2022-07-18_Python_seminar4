764e973231242b808efce9e764a0e8284a865918
Pavel_Ziborov <zzipp@inbox.ru>
2022-07-25 22:49:45 +0300

    Закончил ДЗ к семинару 4

--- zadacha011_DZ.py
+++ zadacha011_DZ.py
@@ -29,70 +29,95 @@
 # Локомотив:2 2 0 0 6
 
 
-
-inputResults = [['Спартак', 9, 'Зенит', 10], ['Локомотив', 12, 'Зенит', 3], ['Спартак', 8, 'Локомотив', 15]]
-
-# N = int(input('Введите число завершенных игр: '))
-# def InputResults(N):                                         # Функция ввода данных
-#     results = []
-#     for i in range (0, N):
-#         results.append(input().replace(' ', '').split(';'))
-#         results[i][1] = int(results[i][1])
-#         results[i][3] = int(results[i][3])
-#     return results
+# Функция ввода данных
+def InputResults(N):
+    results = []
+    for i in range(0, N):
+        results.append(input().replace(' ', '').split(';'))
+        results[i][1] = int(results[i][1])
+        results[i][3] = int(results[i][3])
+    return results
 
 
-# def OutputResults(inputResults):
+# Функция подсчета "Всего_игр Побед Ничьих Поражений Всего_очков"
+def OutputResults(inputResults):
 
-#     for item in inputResults:
-#         count(item)
+    # формирование общего списка
+    ObshiySpisok = []
+    for i in range(0, len(inputResults)):
+        for j in range(0, len(inputResults[i])):
+            ObshiySpisok.append(inputResults[i][j])
 
-# inputResults = InputResults(N)
-print('Исходные данные: ')
-print(inputResults)
+    # Формирование уникального списка
+    UnikalniyObshiySpisok = set(ObshiySpisok)
 
-# формирование общего списка
-ObshiySpisok = []
-for i in range(0, len(inputResults)):
-    for j in range (0, len(inputResults[i])):
-        ObshiySpisok.append(inputResults[i][j])
-print('Общий список: ')
-print(ObshiySpisok)
+    # Формирование команд и "игр всего сыграно"
+    komandy = []
+    igry_vsego = []
+    for i in UnikalniyObshiySpisok:
+        if type(i) == str:
+            komandy.append(i)
+            igry_vsego.append(ObshiySpisok.count(i))
 
-# Формирование уникального списка
-UnikalniyObshiySpisok = set(ObshiySpisok)
+    # Подсчет побед
+    pobedy = []
+    for i in komandy:
+        count = 0
+        for x in range(0, len(inputResults)):
+            if (inputResults[x][2] == i) and (inputResults[x][3] > inputResults[x][1]):
+                count = count + 1
+            elif (inputResults[x][0] == i) and (inputResults[x][1] > inputResults[x][3]):
+                count = count + 1
+            else:
+                continue
+        pobedy.append(count)
 
-# Формирование команд и "игр всего сыграно"
-komandy = []
-igry_vsego = []
-for i in UnikalniyObshiySpisok:
-    if type(i) == str:
-        komandy.append(i)
-        igry_vsego.append(ObshiySpisok.count(i))
+    # Подсчет ничьих
+    nichya = []
+    for i in komandy:
+        count = 0
+        for x in range(0, len(inputResults)):
+            if (inputResults[x][2] == i) and (inputResults[x][3] == inputResults[x][1]):
+                count = count + 1
+            elif (inputResults[x][0] == i) and (inputResults[x][1] == inputResults[x][3]):
+                count = count + 1
+            else:
+                continue
+        nichya.append(count)
 
-# Подсчет побед
-ochkov_vsego = []
-ochkov_vsego_temp = []
-for j in komandy:
-    for i in range(0, len(ObshiySpisok) - 1):
-        if ObshiySpisok[i] == j:
-            ochkov_vsego_temp.append(ObshiySpisok[i+1])
-ochkov_vsego = [ochkov_vsego_temp[i] + ochkov_vsego_temp[i+1] for i in range(0, len(ochkov_vsego_temp)-1) if i%2 == 0] 
+    # Подсчет поражений
+    porazheniya = []
+    for i in komandy:
+        count = 0
+        for x in range(0, len(inputResults)):
+            if (inputResults[x][2] == i) and (inputResults[x][3] < inputResults[x][1]):
+                count = count + 1
+            elif (inputResults[x][0] == i) and (inputResults[x][1] < inputResults[x][3]):
+                count = count + 1
+            else:
+                continue
+        porazheniya.append(count)
+
+    # подсчет очков
+    ochki = []
+    for i in range(0, len(komandy)):
+        ochki.append(pobedy[i] * 3 + nichya[i] * 1)
+
+    return komandy, igry_vsego, pobedy, nichya, porazheniya, ochki
+
+
+# Функция вывода данных на экран
+def PrintOutputResults(results):
+    for k in range(0, len(results[0])):
+        print(
+            f'{results[0][k]} {results[1][k]} {results[2][k]} {results[3][k]} {results[4][k]} {results[5][k]}')
 
 
 
-pobedy = []
-for i in komandy:
-    
-
-
-
-
-print('Команды: ')
-print(komandy)
-print('Всего игр: ')
-print(igry_vsego)
-print('Очков всего: ')
-print(ochkov_vsego)
-
-# print(UnikalniyObshiySpisok)
+# inputResults = [['Спартак', 9, 'Зенит', 10], ['Локомотив', 12, 'Зенит', 3], ['Спартак', 8, 'Локомотив', 15]]
+N = int(input('Введите число завершенных игр:\n'))
+print('Введите результаты матчей:')
+inputResults = InputResults(N)
+results = OutputResults(inputResults)
+print('\nТурнирная таблица:')
+PrintOutputResults(results)
